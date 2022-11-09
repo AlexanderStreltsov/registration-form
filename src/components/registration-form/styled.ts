7af30e100dcf0f53b93497645d9bd98be94de6a5
AlexanderStreltsov <alexander.streltsov91@yandex.ru>
2022-11-09 13:18:27 +0300

    fix: s and m mobile form height

--- src/components/registration-form/styled.ts
+++ src/components/registration-form/styled.ts
@@ -10,6 +10,7 @@ export const StyledFormWrapper = styled(Paper)`
   gap: 12px;
 
   @media ${device.mobileS} {
+    min-height: 100vh;
     max-width: 450px;
     padding: 50px 20px 40px;
     border-top-left-radius: 0 !important;
@@ -19,6 +20,7 @@ export const StyledFormWrapper = styled(Paper)`
   }
 
   @media ${device.mobileL} {
+    min-height: auto;
     padding: 50px 60px 60px;
     border-radius: 8px !important;
   }
