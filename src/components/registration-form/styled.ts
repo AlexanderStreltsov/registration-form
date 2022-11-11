1629bce44743f8d61c9ad77d342e9ff30b178653
AlexanderStreltsov <alexander.streltsov91@yandex.ru>
2022-11-11 17:58:37 +0300

    fix: width form wrapper

--- src/components/registration-form/styled.ts
+++ src/components/registration-form/styled.ts
@@ -21,7 +21,6 @@ export const StyledFormWrapper = styled(Paper)`
   @media ${device.tablet} {
     border-radius: 8px !important;
     padding: 50px 60px 60px;
-    width: 450px;
     box-shadow: 0px 3px 3px -2px rgb(0 0 0 / 20%),
       0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%) !important;
   }
